74117f4c66717798920279c8233ab1364f4c1c47
NabeelSarwar10 <nabeeldotsarwar@gmail.com>
2021-02-11 19:21:35 +0500

    more code

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed')
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/feed', feedRoutes);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorizaton");
+  next();
+});
 
-app.listen(8080);
+app.use("/feed", feedRoutes);
+
+mongoose
+  .connect(
+    "mongodb+srv://Nabeel:PBpod0DW0YksU2O4@cluster0.8hou4.mongodb.net/max?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((result) => {
+    console.log('Database Connected...');
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,19 +1,44 @@
+const { validationResult } = require("express-validator");
+
+const Post = require("../models/post");
+const { post } = require("../routes/feed");
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the First post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the First post!",
+        imageUrl: "images/pubg.png",
+        creator: {
+          name: "Nabeel",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPosts = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-    },
+  const Post = new Post({
+    title: title,
+    content: content,
+    creator: { name: "Nabeel" },
+  });
+  post.save().then((result) => {
+    console.log(result);
+    res.status(201).json({
+      message: "Post created successfully!",
+      post: result
+    });
   });
 };
