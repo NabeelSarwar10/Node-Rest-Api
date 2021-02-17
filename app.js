67250b188b5481279840716c654cbd372a9fb165
NabeelSarwar10 <nabeeldotsarwar@gmail.com>
2021-02-17 18:07:28 +0500

    Added Middleware

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require('path');
-var dotenv = require('dotenv');
+const path = require("path");
+const multer = require("multer");
+var dotenv = require("dotenv");
 dotenv.config();
 
 const feedRoutes = require("./routes/feed");
+const feedMiddleware = require("./middleware/feed");
 const local = process.env.MONGO_URI_LOCAL;
 const atlas = process.env.MONGO_URI_ATLAS;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorizaton");
-  next();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(feedMiddleware.cors);
+app.use(feedMiddleware.errorHandeler);
 app.use("/feed", feedRoutes);
 
-app.use((error, req, res, next)=>{
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  res.status(status).json({message: message});
-})
 
 mongoose
-  .connect(
-    local,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(local, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
-    console.log('Database Connected...');
-    
+    console.log("Database Connected...");
   })
   .catch((err) => {
     console.log(err);
   });
-  app.listen(8080);
+app.listen(8080);
---
+++ middleware/feed.js
@@ -0,0 +1,16 @@
+exports.cors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorizaton");
+  next();
+};
+
+exports.errorHandeler = (error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+};
